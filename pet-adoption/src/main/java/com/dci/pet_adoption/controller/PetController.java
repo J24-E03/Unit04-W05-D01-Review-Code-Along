cc594a33a3548ff339a41a47294a2c23d1135356
omarakamal <omaraakamal@gmail.com>
2025-05-26 11:18:59 +0300

    update

--- pet-adoption/src/main/java/com/dci/pet_adoption/controller/PetController.java
+++ pet-adoption/src/main/java/com/dci/pet_adoption/controller/PetController.java
@@ -3,6 +3,9 @@ package com.dci.pet_adoption.controller;
 import com.dci.pet_adoption.service.PetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,6 +15,23 @@ public class PetController {
 
     private final PetService petService;
 
+
+    @GetMapping
+    private String getAllPets(Model model){
+        model.addAttribute("pets",petService.findAllPets());
+
+        return "pets/index";
+    }
+
+
+    @GetMapping("/{id}")
+    private String getPetById(Model model, @PathVariable Long id){
+
+        model.addAttribute("pet",petService.getPetById(id));
+
+        return "pets/pet-details";
+
+    }
 
 
 }
